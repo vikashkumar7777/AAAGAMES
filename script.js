8aa0f6f70200974943295aaedda092a3a9938914
vikashkumar7777 <vikashkumar372029@gmail.com>
2026-09-13 02:46:37 +0530

    swipe

--- script.js
+++ script.js
@@ -146,13 +146,13 @@ function showSlide(index, direction = 1) {
         // Bring new image into the center
         setTimeout(function() {
 
-            featuredImage.style.transition = "transform 0.6s ease";
+            featuredImage.style.transition = "transform 0.7s ease";
 
             featuredImage.style.transform = "translateX(0)";
 
         }, 50);
 
-    }, 300);
+    }, 350);
 
 
     // Update dots
@@ -176,7 +176,7 @@ setInterval(function() {
 
     showSlide(currentSlide, 1);
 
-}, 3500);
+}, 4000);
 
 
 // NEXT BUTTON
